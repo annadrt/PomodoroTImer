6ffef84f0e0bbc707d65425a162e61b2fb72cb68
annadrt <annaduarte2219@gmail.com>
2026-05-15 12:59:01 -0300

    chore: remove arquivos de rascunho

--- learn.py
+++
@@ -1,9 +0,0 @@
-nomeSessao = input("Sessão: ")
-duracao = int(input("Duração: "))
-pausa = int(input("Pausa: "))
-quantidadeSessoes = int(input("Sessões: "))
-
-print(f"Sessão: {nomeSessao}")
-print(f"Duração: {duracao} minutos")
-print(f"Pausa:{pausa} minutos")
-print(f"Sessões: {quantidadeSessoes}")
