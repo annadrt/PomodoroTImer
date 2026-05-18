d82f0ff99002dbaa7b3fffacdeb2e6c4fe7319bf
annadrt <annaduarte2219@gmail.com>
2026-05-18 20:52:14 -0300

    wip:interface

--- interface.py
+++ interface.py
@@ -1,25 +1,48 @@
 import tkinter as tk
-
-def sessionSettings():
-    duracao = duracao.get()
-    pausa = pausa.get()
-    sessao = sessao.get()
-
-    print("Tempo da sessão: " + duracao)
-    print("Tempo da pausa: " + pausa)
-    print("Número de sessões:" + sessao)
+from pomodoro import rodar_sessao, rodar_pausa
 
 root = tk.Tk()
 root.geometry("600x400")
 root.title("Pomodoro Timer")
 
+def tick_pausa(tempo_restante, pausa, sessao):
+    if tempo_restante > 0:
+        print(f"Tempo da pausa: {pausa}")
+        root.after(1000, lambda: tick(pausa -1))
+        
 
-startButton = tk.Button(root, text ='Start.', command = sessionSettings)
+def tick(tempo_restante, pausa, sessao, nomeSessoes, sessaoAtual):
+    if tempo_restante > 0:
+        print(f"Tempo restante:  {tempo_restante}")
+        root.after(1000, lambda: tick(tempo_restante -1))
+    else:
+        print("Finish.")
 
+def startFunction():
+    duracao = int(entry_duracao.get())
+    print(duracao)
+    pausa = int(entry_pausa.get())
+    print(pausa)
+    sessao = int(entry_sessao.get())
+    print(sessao)
+    tick(duracao)
+    
+
+
+startButton = tk.Button(root, text ='Start.', command = startFunction)
+
+duracao_label = tk.Label(root, text = 'Tempo da sessão: ')
 entry_duracao = tk.Entry(root)
+
+pausa_label = tk.Label(root, text = 'Tempo da pausa: ')
 entry_pausa = tk.Entry(root)
+
+sessao_label = tk.Label(root, text = 'Quantidade de sessões: ')
 entry_sessao = tk.Entry(root)
 
+duracao_label.pack()
+pausa_label.pack()
+sessao_label.pack()
 entry_duracao.pack()
 entry_pausa.pack()
 entry_sessao.pack()
